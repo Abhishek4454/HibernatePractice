d91034e212e1bc25c5eff121e5aec29866b98a39
abhk0721 <abhk0721@WS-15319.netcracker.com>
2023-03-06 15:35:23 +0530

    Native Queries

--- src/main/java/com/coding/HibernateDemo/App.java
+++ src/main/java/com/coding/HibernateDemo/App.java
@@ -3,8 +3,11 @@ package com.coding.HibernateDemo;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
+import org.hibernate.Criteria;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -12,6 +15,7 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 import org.hibernate.service.ServiceRegistry;
+
 
 public class App {
 	public static void main(String[] args) {
@@ -48,7 +52,28 @@ public class App {
 		
 		System.out.println(totalSalary);
 		
-
+		int b=50000;
+		Query query4= session.createQuery("select sum(salary) from Employee where salary< "+b);
+	    Long totalSalary1=(Long) query4.uniqueResult();
+		
+		System.out.println(totalSalary1);
+		
+		Query query5= session.createQuery("select sum(salary) from Employee where salary< :b");
+		query5.setParameter("b", b);
+	    Long totalSalary2=(Long) query5.uniqueResult();
+		
+		System.out.println(totalSalary2);
+		
+		//Native Query
+		SQLQuery query6=session.createSQLQuery("Select eid,salary from employee e where e.eid < 125");
+		query6.setResultTransformer(Criteria.ALIAS_TO_ENTITY_MAP);
+		List employees1=query6.list();
+		
+		for(Object o: employees1) {
+			Map m= (Map)o;
+			System.out.println(m.get("eid")+" : "+m.get("salary"));
+		}
+		
 //		Random r=new Random();
 //		for(int i=100;i<150;i++) {
 //			emp= new Employee();
